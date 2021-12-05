b366a50461c59238ea7a40e8c035eba70fc74d71
ishu <ishgarg19@gmail.com>
2021-12-05 15:34:45 +0530

    changes

--- src/main/java/com/learn/KafkaProject/Producer1.java
+++ src/main/java/com/learn/KafkaProject/Producer1.java
@@ -10,8 +10,8 @@ import org.apache.kafka.common.serialization.StringSerializer;
 public class Producer1 {
 
 	public static void main(String[] args) {
-		
-		String bootstrapServers="127.0.0.1:9091";
+	
+		String bootstrapServers="127.0.0.1:9092";
 		Properties prop=new Properties();
 		prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 		prop.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
